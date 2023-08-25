e532248474b77ecb3f4bf6581d6857ed512ccbd4
rtm713 <rtm713@gmail.com>
2023-08-24 21:12:10 -0500

    added nav structure and updated html title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+
+import Navbar from './components/Navbar'
+import './App.css'
 
 
 function App() {
@@ -6,7 +8,7 @@ function App() {
 
   return (
     <>
-
+      <Navbar/>
     </>
   )
 }
