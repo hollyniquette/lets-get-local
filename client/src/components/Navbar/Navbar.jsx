5638b427af4bad0b8cf38d640c8ee6348f1f0d48
rtm713 <rtm713@gmail.com>
2023-08-24 20:20:53 -0500

    added component folder and Navbar and Post Components

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Navbar() {
+
+    return(
+        <header>
+            <div>
+                //logo
+            </div> 
+            <nav>
+                <a href="">Search</a>
+                <a href="">Profile</a>
+                <button>Logout</button>
+            </nav>
+        </header>
+    )
+
+}
