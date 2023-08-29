dcc3136ee8489ba23c5611cce394cc75805bb189
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-28 21:00:26 -0500

    added local environment variables and added mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const { resolvers } = require('./resolvers.js');
-const { typeDefs } = require('./models/typeDefs.js');
+require("dotenv").config();
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { resolvers } = require("./resolvers.js");
+const { typeDefs } = require("./models/typeDefs.js");
 
 const server = new ApolloServer({ typeDefs, resolvers });
+const mongo_uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
