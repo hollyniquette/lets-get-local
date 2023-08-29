1b07e3d62d098541fb2c6033f0227fac79c08987
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-29 17:46:10 -0500

    atlasdb connected, started on resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,32 @@
+const { Event } = require('./models/Event');
+const { User } = require('./models/User');
+
 // GraphQL Resolvers
 const resolvers = {
   Query: {
     greetings: () => "Let's get local!",
+    events: async () => await Event.find(),
+    users: async () => await User.find(),
+    event: async (parent, args) => await Event.findById(args.id),
+    user: async (parent, args) => await User.findById(args.id),
+  },
+  Mutation: {
+    createEvent: async (parent, args) => {
+      const { name, description, date, time, location, image, category } = args;
+      const event = new Event({
+        name,
+        description,
+        date,
+        time,
+        location,
+        image,
+        category,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      });
+      await event.save();
+      return event;
+    },
   },
 };
 
--- server/server.js
+++ server/server.js
@@ -3,13 +3,25 @@ const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const { resolvers } = require('./resolvers.js');
 const { typeDefs } = require('./models/typeDefs.js');
+const { default: mongoose } = require('mongoose');
+
+const mongo_uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.MONGO_URL}`;
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-const mongo_uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}`;
-
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+mongoose
+  .connect(mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    startStandaloneServer(server, {
+      listen: { port: 4000 },
+    }).then(({ url }) => {
+      console.log(`Server ready at ${url}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Error: ', err.message);
+  });
