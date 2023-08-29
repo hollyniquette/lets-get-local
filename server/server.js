82fe139e22f9ca150a3907ab5485fd29cad66282
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-08-28 20:31:00 -0500

    added typeDefs base resolver and beginning server.js code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-//server file
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { resolvers } = require('./resolvers.js');
+const { typeDefs } = require('./models/typeDefs.js');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
