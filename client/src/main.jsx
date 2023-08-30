304760ff9f57bb20b18c85e9cf6b68e2da1ff50d
rtm713 <rtm713@gmail.com>
2023-08-29 19:39:35 -0500

    added profile page and components, added routing, and added new event page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import HomePage from './components/HomePage.jsx'
+import ProfilePage from './components/ProfilePage.jsx'
+import NewEvent from './components/NewEvent.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+      {
+        path: 'profile',
+        element: <ProfilePage />,
+      },
+      {
+        path: 'profile/newevent',
+        element: <NewEvent />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
