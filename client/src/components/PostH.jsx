35ed5aafec8fc245b5a3a01640e4445c2e8586e7
rtm713 <rtm713@gmail.com>
2023-09-04 00:39:30 -0500

    added switch statement for rendering the correct tag on each post

--- client/src/components/PostH.jsx
+++ client/src/components/PostH.jsx
@@ -1,16 +1,45 @@
 import AutomotiveTag from "./Tags/AutomotiveTag";
+import EducationalTag from "./Tags/EducationalTag";
+import PetsTag from "./Tags/PetsTag";
+import EntertainmentTag from "./Tags/EntertainmentTag";
+import SportsTag from "./Tags/SportsTag";
+import OtherTag from "./Tags/OtherTag";
 
 export default function PostH(props) {
   const { eventName, eventDescription, eventLocation, eventType, eventImage, eventDate, eventTime} =
     props;
 
+    let type = '';
+    switch (eventType) {
+      case 'AUTOMOTIVE':
+        type = <AutomotiveTag/>;
+        break;
+      case 'EDUCATION':
+        type = <EducationalTag/>;
+        break;
+      case 'PETS':
+        type = <PetsTag/>;
+        break;
+      case 'ENTERAINMENT':
+        type = <EntertainmentTag/>;
+        break;
+      case 'SPORTS':
+        type = <SportsTag/>;
+        break;
+      case 'OTHER':
+        type = <OtherTag/>;
+        break;
+    }
+
   return (
     <>
       <div className="homePost">
         <h2>{eventName}</h2>
+        <div>
+          {type}
+        </div>
         <img src={`public/images/${eventImage}.jpg`} />
         <p>{eventDescription}</p>
-        <AutomotiveTag />
         <p>{`Event is on ${eventDate} @ ${eventTime}`}</p>
         <p>{`Location: ${eventLocation}`}</p>
       </div>
