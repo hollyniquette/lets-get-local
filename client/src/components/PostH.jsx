b7c35f4afeb7bee650ed2ad2aaa4a9a3ee43d01d
rtm713 <rtm713@gmail.com>
2023-09-04 00:52:57 -0500

    added images to switch statement to render correct images based on type

--- client/src/components/PostH.jsx
+++ client/src/components/PostH.jsx
@@ -10,24 +10,31 @@ export default function PostH(props) {
     props;
 
     let type = '';
+    let image = '';
     switch (eventType) {
       case 'AUTOMOTIVE':
         type = <AutomotiveTag/>;
+        image = 'auto_720.jpg';
         break;
       case 'EDUCATION':
         type = <EducationalTag/>;
+        image = 'education_720.jpg';
         break;
       case 'PETS':
         type = <PetsTag/>;
+        image = 'pets_720.jpg';
         break;
       case 'ENTERAINMENT':
         type = <EntertainmentTag/>;
+        image = 'entertainment_720.jpg';
         break;
       case 'SPORTS':
         type = <SportsTag/>;
+        image = 'sport_720.jpg';
         break;
       case 'OTHER':
         type = <OtherTag/>;
+        image = 'other_720.png';
         break;
     }
 
@@ -38,7 +45,7 @@ export default function PostH(props) {
         <div>
           {type}
         </div>
-        <img src={`public/images/${eventImage}.jpg`} />
+        <img src={`public/images/${image}`} />
         <p>{eventDescription}</p>
         <p>{`Event is on ${eventDate} @ ${eventTime}`}</p>
         <p>{`Location: ${eventLocation}`}</p>
