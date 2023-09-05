fce55b24725c4582e0e3166bf67bacc49bf60406
TiffanySwank <127704686+TiffanySwank@users.noreply.github.com>
2023-09-05 16:18:11 -0500

    Update LoginPage.jsx

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function ProfilePage() {
   return (
     <>
+      <div class="container">
       <h1>Welcome To Lets Get Local</h1>
       <Link to="login">
         <button>Login</button>
@@ -10,6 +11,7 @@ export default function ProfilePage() {
       <Link to="signup">
         <button>Sign Up</button>
       </Link>
+      </div>
     </>
   );
 }
