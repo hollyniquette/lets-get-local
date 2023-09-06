9f82f1c94670743c011af79a035a3192c8603b89
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-09-05 21:22:26 -0500

    heroku dumb

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
+  uri: `${process.env.API_HOST}/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
-require('dotenv').config();
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const { resolvers } = require('./resolvers.js');
-const { typeDefs } = require('./models/typeDefs.js');
-const { default: mongoose } = require('mongoose');
-
+require("dotenv").config();
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { resolvers } = require("./resolvers.js");
+const { typeDefs } = require("./models/typeDefs.js");
+const { default: mongoose } = require("mongoose");
 
 const mongo_uri = `mongodb+srv://root:${process.env.PASSWORD}@${process.env.MONGO_URL}`;
-
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -17,15 +15,15 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connected to MongoDB Atlas');
+    console.log("Connected to MongoDB Atlas");
     startStandaloneServer(server, {
-      listen: { port: 4000 },
+      listen: { port: process.env.PORT || 4000 },
     }).then(({ url }) => {
       console.log(`Server ready at ${url}`);
     });
   })
   .catch((err) => {
-    console.log('Error connecting to MongoDB Atlas');
-    console.log('Error: ', err.message);
-    console.log('Shutting down server');
+    console.log("Error connecting to MongoDB Atlas");
+    console.log("Error: ", err.message);
+    console.log("Shutting down server");
   });
