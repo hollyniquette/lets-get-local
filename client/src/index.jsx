9ffcb8b8f88292ba8416fdc722ab650c1be9b966
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-09-05 22:14:54 -0500

    come on

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
