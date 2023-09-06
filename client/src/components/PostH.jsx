63c15c4547735569a15a4a306572f1a040a5d56b
hollyniquette <127768285+hollyniquette@users.noreply.github.com>
2023-09-06 07:37:14 -0500

    trying to get images to load on heroku

--- client/src/components/PostH.jsx
+++ client/src/components/PostH.jsx
@@ -6,46 +6,51 @@ import SportsTag from "./Tags/SportsTag";
 import OtherTag from "./Tags/OtherTag";
 
 export default function PostH(props) {
-  const { eventName, eventDescription, eventLocation, eventType, eventImage, eventDate, eventTime} =
-    props;
+  const {
+    eventName,
+    eventDescription,
+    eventLocation,
+    eventType,
+    eventImage,
+    eventDate,
+    eventTime,
+  } = props;
 
-    let type = '';
-    let image = '';
-    switch (eventType) {
-      case 'AUTOMOTIVE':
-        type = <AutomotiveTag/>;
-        image = 'auto_720.jpg';
-        break;
-      case 'EDUCATION':
-        type = <EducationalTag/>;
-        image = 'education_720.jpg';
-        break;
-      case 'PETS':
-        type = <PetsTag/>;
-        image = 'pets_720.jpg';
-        break;
-      case 'ENTERAINMENT':
-        type = <EntertainmentTag/>;
-        image = 'entertainment_720.jpg';
-        break;
-      case 'SPORTS':
-        type = <SportsTag/>;
-        image = 'sport_720.jpg';
-        break;
-      case 'OTHER':
-        type = <OtherTag/>;
-        image = 'other_720.png';
-        break;
-    }
+  let type = "";
+  let image = "";
+  switch (eventType) {
+    case "AUTOMOTIVE":
+      type = <AutomotiveTag />;
+      image = "auto_720.jpg";
+      break;
+    case "EDUCATION":
+      type = <EducationalTag />;
+      image = "education_720.jpg";
+      break;
+    case "PETS":
+      type = <PetsTag />;
+      image = "pets_720.jpg";
+      break;
+    case "ENTERAINMENT":
+      type = <EntertainmentTag />;
+      image = "entertainment_720.jpg";
+      break;
+    case "SPORTS":
+      type = <SportsTag />;
+      image = "sport_720.jpg";
+      break;
+    case "OTHER":
+      type = <OtherTag />;
+      image = "other_720.png";
+      break;
+  }
 
   return (
     <>
       <div className="homePost">
         <h2>{eventName}</h2>
-        <div>
-          {type}
-        </div>
-        <img src={`public/images/${image}`} />
+        <div>{type}</div>
+        <img src={`/public/images/${image}`} />
         <p>{eventDescription}</p>
         <p>{`Event is on ${eventDate} @ ${eventTime}`}</p>
         <p>{`Location: ${eventLocation}`}</p>
